857470a9020e57e927c18b20b66686c053a5e59b
kelq <kel_quek@hotmail.com>
2015-11-25 00:07:59 +0800

    Update ui.R
    Didn't realize must upload server.R and ui.R.
    Uploaded the exact copies used for the final submission of the Shiny App.

--- ui.R
+++ ui.R
@@ -1,40 +1,48 @@
 library(shiny)
 
-# Define UI for slider demo application
 shinyUI(fluidPage(
+  titlePanel("Would you be getting a Christmas present this year?"),
   
-  #  Application title
-  titlePanel("Sliders"),
-  
-  # Sidebar with sliders that demonstrate various available
-  # options
   sidebarLayout(
     sidebarPanel(
-      # Simple integer interval
-      sliderInput("integer", "How much do you believe in Santa Claus?", min=0, max=1000, value=500),
+      helpText("This app estimates the probability that you will receive a Christmas present this year."),            
+      br(),            
+
+      sliderInput("num_belief", label = h5("How much do you believe in Santa Claus?
+                                           (0 - Don't believe, 4 - Absolutely worships him"), min=0, max=4, value=2),
+      br(),     
       
-      # Decimal interval with step value
-      sliderInput("decimal", "Decimal:", min = 0, max = 1, value = 0.5, step= 0.1),
+      selectInput("num_naughty", label = h5("How naughty have you been this year?"), 
+                  choices = list("Absolutely criminal" = 0, "Bordering on breaking the law" = 1, "Cheeky at times" = 2, "As obedient as a dog" = 3),
+                  selected = 3),
+      br(),            
       
-      # Specification of range within an interval
-      sliderInput("range", "How many kind deeds did you do this year?", min = 1, max = 1000, value = c(200,500)),
-      
-      # Provide a custom currency format for value display, 
-      # with basic animation
-      sliderInput("format", "Custom Format:", min = 0, max = 10000, value = 0, step = 2500, format="$#,##0", locale="us", animate=TRUE),
-      
-      # Animation with custom interval (in ms) to control speed,
-      # plus looping
-      sliderInput("animation", "Looping Animation:", 1, 2000, 1, step = 10, animate=animationOptions(interval=300, loop=TRUE)),
+      radioButtons("num_chimney", label = h5("Do you have a chimney or some other way that Santa could get in?"), choices=list("Yes" = 1, "No" = 0), selected = 1),
       br(),
-      actionButton("goButton", "Go!"),
-      
-      p("Click the button to update the value displayed in the main panel.")
-    ),
+
+      br(),            
+      actionButton("action_odds", label = "Compute my odds!")        
+      ),
     
-    # Show a table summarizing the values entered
     mainPanel(
-      tableOutput("values")
-    )
-  )
-))
+      tabsetPanel(
+        tabPanel("Your Odds",
+                 br(),
+                 textOutput("str_odds")
+        ),
+        tabPanel("Documentation",
+                 p(h4("Would you get a Christmas Present?")),
+                 br(),
+                 helpText("This app estimates the probability that you will receive a Christmas present this year."),
+                 HTML("<br>The odds are computed from:<br><br>
+                      - <b>Whether you believe in Santa Claus</b><br>
+                        <i>Slide the slider to indicate your level of belief. The higher the belief, the more you slide to the right.</i> <br><br>
+
+                      - <b>Whether you have been naughty this year</b><br>
+                        <i>Information will be kept strictly confidential.</i><br><br>
+ 
+                      - <b>Whether you have a chimney, or something similar for Santa to handover the present</b><br>
+                        <i>e.g. Something similar could be a big mailbox, UberRUSH</i><br>
+                        <i>If not, you are definitely out of luck. Try asking your parents. </i><br><br>
+                      ")                
+                 ))))))
